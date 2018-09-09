b69b50d2ad1966227c568fb3001b7561f2edc92b
Lindqvist Oliver <olindqvi@vdi-cubbli-060.ad.helsinki.fi>
2018-09-09 14:16:52 +0300

    Tehtävä kahdeksan(8)

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -66,6 +66,7 @@ class BreweriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_brewery
       @brewery = Brewery.find(params[:id])
+      render :show
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
