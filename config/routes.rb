c8e61835f57431833be63069b94418344ca6243b
Lindqvist Oliver <olindqvi@vdi-cubbli-071.ad.helsinki.fi>
2018-09-11 15:08:47 +0300

    Start of raitings system

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   resources :beers
   resources :breweries
   root 'breweries#index'
+
+  get 'kaikki_bisset', to: 'beers#index'
+
+  get 'raitings', to: 'raitings#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
