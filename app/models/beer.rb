3f278e00773e9593a2a24c2a46ed2801997becd0
Lindqvist Oliver <olindqvi@vdi-cubbli-064.ad.helsinki.fi>
2018-09-13 15:26:52 +0300

    V2T5 ja V2T6: SE osaa jo taivuttaa sanoja! Epäilen että se muuttuu kohta tietoseksi itsestäängit status Ehkä pitäisi jo irrottaa varuiksi tulppa!

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,7 +3,7 @@ class Beer < ApplicationRecord
   has_many :raitings
 
   def avarage_raiting
-    a = 0
+    a = 0.0
     Raiting.where(beer_id: self.id).find_each do |koira|
       a += koira.score
       puts "aa #{koira.score}"
@@ -11,7 +11,21 @@ class Beer < ApplicationRecord
     return (a)/  Raiting.where(beer_id: self.id).count
   end
 
+  def avarage_raiting2
+    a = 0.0
+    Raiting.where(beer_id: self.id).map{|jotain| a += jotain.score}
+    return (a)/  Raiting.where(beer_id: self.id).count
+  end
 
+  def avarage_raiting3
+    a = 0.0
+    a= Raiting.where(beer_id: self.id).map{|jotain| jotain.score}.reduce(:+)
+    return (a)/  Raiting.where(beer_id: self.id).count
+  end
+
+  def avarage_raiting4
+    return self.raitings.map{|jotain| jotain.score}.reduce(:+)/  self.raitings.count
+  end
 
 def count_raitings
   return Raiting.where(beer_id: self.id).count
