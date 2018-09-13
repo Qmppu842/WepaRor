4fc0bb2650aa8c5b11c679e4dbd8b68a11abb441
Lindqvist Oliver <olindqvi@vdi-cubbli-064.ad.helsinki.fi>
2018-09-13 18:15:39 +0300

    Added redirect and made not-so-possible to enter incorrect raitings/beer ids

--- app/controllers/raitings_controller.rb
+++ app/controllers/raitings_controller.rb
@@ -1,6 +1,15 @@
 class RaitingsController < ApplicationController
-    def index
-      @raitings = Raiting.all
-    end
+  def index
+    @raitings = Raiting.all
+  end
+
+  def new
+     @raiting = Raiting.new
+  end
+
+  def create
+    Raiting.create params.require(:raiting).permit(:score, :beer_id)
+    redirect_to raitings_path
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
+
+
   root 'breweries#index'
 
+
+  #Beer relaited routes
   get 'kaikki_bisset', to: 'beers#index'
 
+
+  #Raitings relaited routes
   get 'raitings', to: 'raitings#index'
+  get 'ratings', to: 'raitings#index'
+  get 'raitings/new', to:'raitings#new'
+  get 'ratings/new', to:'raitings#new'
+  post 'raitings' , to: 'raitings#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
