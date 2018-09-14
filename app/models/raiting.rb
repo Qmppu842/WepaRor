316641ea166852580d9be25a36641292569e38f6
Lindqvist Oliver <olindqvi@vdi-cubbli-070.ad.helsinki.fi>
2018-09-14 15:26:49 +0300

    Please do not use this! It has endless recursio!

--- app/models/raiting.rb
+++ app/models/raiting.rb
@@ -2,6 +2,11 @@ class Raiting < ApplicationRecord
   belongs_to :beer
 
   def to_s
-    return "#{Beer.find_by( id: self.beer_id).name } #{self.score}"
+    begin
+      return "#{Beer.find_by( id: self.beer_id).name } #{self.score}"
+    rescue NoMethodError
+      self.destroy
+    end
+    self.to_s
   end
 end
