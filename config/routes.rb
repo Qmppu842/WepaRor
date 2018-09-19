33f4ff2ee141f5aed9d0ea7c99daf40bfccf2af0
Lindqvist Oliver <olindqvi@vdi-cubbli-055.ad.helsinki.fi>
2018-09-19 16:02:06 +0300

    Made easier to not not get to sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   # User relaited things
   get 'signup', to:'users#new'
-
+  get 'singup', to:'users#new'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
