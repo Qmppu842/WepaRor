9d9596e1982b907f8bd79d31c4502054c8f74ea8
Lindqvist Oliver <olindqvi@vdi-cubbli-070.ad.helsinki.fi>
2018-09-30 23:44:39 +0300

    Travis test

--- spec/features/beer_page_spec.rb
+++ spec/features/beer_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 include Helpers
-
+# Commit for travis
 describe "Beer" do
   let!(:brewery){ FactoryBot.create :brewery}
   let!(:brewery2){ FactoryBot.create :brewery, name: "Kirahvi"}
