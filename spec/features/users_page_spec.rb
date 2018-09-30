dd58c9c8008cc7c3773132e42b81831fba6b8ce1
Lindqvist Oliver <olindqvi@vdi-cubbli-070.ad.helsinki.fi>
2018-09-30 22:45:06 +0300

    V4T7: Like a targetshooter

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -34,5 +34,43 @@ describe "User" do
     }.to change{User.count}.by(1)
   end
 
+  describe "making ratings" do
+      let!(:user2){FactoryBot.create(:user, username:"Velma")}
+      let!(:user1){FactoryBot.create(:user, username:"Vilma")}
+
+      it "but just cheking empty situtiation" do
+        sign_in(username: "Vilma", password: "Foobar1")
+        visit user_path(user1)
+        expect(page).to have_content("This user has given premission to sell their soul.")
+      end
+
+      it "and then changing account" do
+        sign_in(username: "Vilma", password: "Foobar1")
+        create_beers_with_many_ratings({user: user1}, 5, 20, 10, 5)
+        visit user_path(user1)
+
+        expect(page).to have_content("This user has given 4 raitings with average of 10")
+
+        sign_in(username: "Velma", password: "Foobar1")
+        visit user_path(user2)
+        expect(page).to have_content("This user has given premission to sell their soul.")
+      end
+
+      it "and then changing account and making ratings again" do
+        sign_in(username: "Vilma", password: "Foobar1")
+        create_beers_with_many_ratings({user: user1}, 5, 20, 10, 5)
+        visit user_path(user1)
+
+        expect(page).to have_content("This user has given 4 raitings with average of 10")
+
+        sign_in(username: "Velma", password: "Foobar1")
+        create_beers_with_many_ratings({user: user2}, 15, 30, 20, 15)
+        visit user_path(user2)
+      expect(page).to have_content("This user has given 4 raitings with average of 20")
+      end
+
+
+  end
+
 
 end
