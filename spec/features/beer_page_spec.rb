f65e6cbbddc1ac8aae92495cbc0a8744ecd8a65e
Lindqvist Oliver <olindqvi@vdi-cubbli-070.ad.helsinki.fi>
2018-09-30 21:49:52 +0300

    V4T5: I wish I had more brain cells

---
+++ spec/features/beer_page_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+include Helpers
+
+describe "Beer" do
+  let!(:brewery){ FactoryBot.create :brewery}
+  let!(:brewery2){ FactoryBot.create :brewery, name: "Kirahvi"}
+  let!(:user){FactoryBot.create :user}
+
+
+  it "can be added with valid data" do
+    visit new_beer_path
+
+    fill_in('beer_name', with:'Expresso')
+    select('Lager', from: 'beer[style]')
+    select('Kirahvi', from: 'beer[brewery_id]')
+
+    expect{
+      click_button("Create Beer")
+    }.to change{Beer.count}.from(0).to(1)
+  end
+
+  it "cant be added with invalid data" do
+    visit new_beer_path
+
+    #fill_in('beer_name', with:'')
+    select('Lager', from: 'beer[style]')
+    select('Kirahvi', from: 'beer[brewery_id]')
+
+    expect(Beer.count).to eq(0)
+  end
+
+
+
+end
