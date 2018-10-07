48a21209e5ba5fa70554a2b7e2e26d2addfdfd67
Lindqvist Oliver <olindqvi@vdi-cubbli-067.ad.helsinki.fi>
2018-10-07 20:23:56 +0300

    V5T5 ja V5T6: Some tests broke, 5 character addition fixed them

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,21 @@ class PlacesController < ApplicationController
   def index
   end
 
+  def show
+    cite = session[:current_city]
+    cite = cite.downcase
+    res_id = params[:id]
+    jotain = Rails.cache.read cite
+    restaurante = nil
+    jotain.each do |thing|
+      if thing.id == res_id
+        restaurante = thing
+      end
+    end
+    # byebug
+    @restaurant = restaurante
+  end
+
   def search
     @places = BeermappingApi.places_in(params[:city])
     if @places.empty?
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :breweries
   resources :raitings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
+  # Why recources are not working? I got no idea
+  # recources :places, only: [:index, :show]
   root 'breweries#index'
 
 
@@ -29,9 +31,10 @@ Rails.application.routes.draw do
   delete 'signout', to: 'sessions#destroy'
   delete 'singout', to: 'sessions#destroy'
 
-  # Places
+  # Places relaited things
   get 'places', to: 'places#index'
   post 'places', to: 'places#search'
+  get 'places/:id', to:'places#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
