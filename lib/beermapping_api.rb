5aa7431904e7598b3a10065cc125cec421a481e5
Lindqvist Oliver <olindqvi@vdi-cubbli-067.ad.helsinki.fi>
2018-10-07 13:14:32 +0300

    Väliaika commit kaiketi

---
+++ lib/beermapping_api.rb
@@ -0,0 +1,19 @@
+class BeermappingApi
+  def self.places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    places = response.parsed_response["bmp_locations"]["location"]
+
+    return [] if places.is_a?(Hash) && places['id'].nil?
+
+    places = [places] if places.is_a?(Hash)
+    @places = places.map do |place|
+      Place.new(place)
+    end
+  end
+
+  def self.key
+    "0dbbed8d7e5ea1260942433afb62faf5"
+  end
+end
