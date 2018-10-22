682e005ba039d4662455bec9b4852f7c7273d7dc
Lindqvist Oliver <olindqvi@vdi-cubbli-072.ad.helsinki.fi>
2018-10-22 20:01:29 +0300

    V7T3

--- app/assets/javascripts/brewerylist.js
+++ app/assets/javascripts/brewerylist.js
@@ -1,7 +1,7 @@
 const BREWERIES = {}
 
 BREWERIES.show = () => {
-  $("#breweriestable tr:gt(0)").remove()
+  $("#brewerytable tr:gt(0)").remove()
   const table = $("#brewerytable")
 
   BREWERIES.list.forEach((brewery) =>{
@@ -20,7 +20,21 @@ BREWERIES.show = () => {
     table.append(future)
   })
 }
-
+BREWERIES.sort_by_name = () =>{
+  BREWERIES.list.sort((a,b) => {
+    return a.name.toUpperCase().localeCompare(b.name.toUpperCase())
+  })
+}
+BREWERIES.sort_by_year = () =>{
+  BREWERIES.list.sort((a,b) => {
+    return b.year - a.year
+  })
+}
+BREWERIES.sort_by_beers = () =>{
+  BREWERIES.list.sort((a,b) => {
+    return b.beers - a.beers
+  })
+}
 document.addEventListener("turbolinks:load", () => {
   console.log("moiu")
   if ($("#brewerytable").length == 0) {
@@ -29,7 +43,23 @@ document.addEventListener("turbolinks:load", () => {
 
   $("#name").click((e) =>{
     e.preventDefault()
-
+    BREWERIES.sort_by_name()
+    BREWERIES.show()
+  })
+  $("#year").click((e) =>{
+    e.preventDefault()
+    BREWERIES.sort_by_year()
+    BREWERIES.show()
+  })
+  $("#beers").click((e) =>{
+    e.preventDefault()
+    BREWERIES.sort_by_beers()
+    BREWERIES.show()
+  })
+  $("#active").click((e) =>{
+    e.preventDefault()
+    BREWERIES.sort_by_name()
+    BREWERIES.show()
   })
 
 
